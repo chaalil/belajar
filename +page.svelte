5b006147594b27a5fc3c920d0872a023c7fd86e2
chaalil <123705943+chaalil@users.noreply.github.com>
2023-01-27 08:02:04 +0700

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,52 @@
+<script>
+    import 'material-symbols/sharp.css'
+</script>
+    <header>
+        <a href="">
+            <i class="mdi mdi-home-assistant"></i>
+            home
+        </a>
+        <a href="galery">
+            <i class="mdi mdi-image"></i>
+            galeri
+        </a>
+        <a href="thullab">
+            <i class="mdi mdi-account-school"></i>
+            Thullab
+        </a>
+        <a href="kenangan">
+            <i class="mdi mdi-camera"></i>
+            kenangan
+        </a>    
+    </header>
+<style>
+    header{
+        background-color: rgb(172, 227, 168);
+        display: flex;
+        font-weight:normal;
+        gap: 3rem;
+        justify-content: center;
+        padding: 1.3rem;
+        justify-items: center;
+        position: fixed;
+        width: 100%;
+        text-transform: capitalize;
+        font-family: Verdana, Tahoma, sans-serif;
+        font-size: 20px;
+    }
+    a{
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        color: rgba(30, 36, 57, 0.943);
+        gap: 10px;
+        transition: all;
+        transition-duration: 1000ms;
+    }
+    a:hover{
+        color: rgb(78, 80, 196);
+        scale: 1.3;
+        transition: all;
+        transition-duration: 1000ms;
+    }
+</style>
